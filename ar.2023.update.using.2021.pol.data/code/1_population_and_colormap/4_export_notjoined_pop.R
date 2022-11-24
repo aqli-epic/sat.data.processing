4c107c8246a86609274258bd7b1c148395e706c7
AarshBatra <aarshbatra.in@gmail.com>
2022-11-24 23:27:00 +0530

    updated 4_export_notjoined_pop.R, next step join the not_joined points in QGIS

--- ar.2023.update.using.2021.pol.data/code/1_population_and_colormap/4_export_notjoined_pop.R
+++ ar.2023.update.using.2021.pol.data/code/1_population_and_colormap/4_export_notjoined_pop.R
@@ -12,48 +12,49 @@
 
 #
 
-# Runtime: 1 minute
+# Runtime: 1 minute (locally)
 
 ###############################################
 
 
-
+# loading libraries
 library(sf)
-
 library(dplyr)
 
 
+# population year
+pop_year <- 2021
 
-pop_year <- 2019
-
-
-
-pop_dir <- "C:/Arc/Preserve/data/population/intermediate/pop_points_joined_notjoined/notjoined"   
-
-
-
+# loop start and end indices (equal to the total number of tiles in which the population raster was divided initially)
 start_i <- 1
 
 end_i <- 25
 
 
-
+# setting a variable to distinguish first iteration of the for loop from the rest of the iterations
 first_in <- TRUE
 
 
+# running a for loop through all not joined points shapefiles and combine them into a single "not_joined" master shape file, which will
+# then be further processed in QGIS in the next step
+for (i in c(start_i:end_i)) {
 
-for (i in c(start_i:end_i)) {	
+  # check if file exists
+	if(file.exists(file.path("./ar.2023.update.using.2021.pol.data/data/intermediate/1_population_and_colormap/3_join_pop_to_shp/not_joined",
+	                         "/pop_notjoined", toString(i), ".shp", fsep = ""))){
+    print(i)
+	  # read in file if it exists
+		chunk <- st_read(file.path("./ar.2023.update.using.2021.pol.data/data/intermediate/1_population_and_colormap/3_join_pop_to_shp/not_joined",
+		                           "/pop_notjoined", toString(i), ".shp", fsep = ""))
 
-	if(file.exists(file.path(pop_dir, "/", pop_year, "/pop_notjoined_", toString(i), ".shp", fsep = ""))){
-
-		chunk <- st_read(file.path(pop_dir, "/", pop_year, "/pop_notjoined_", toString(i), ".shp", fsep = ""))
-
+		# if its the first file, read it in
 		if(first_in){
 
 			altogether <- chunk
 
 			first_in <- FALSE
 
+      # for all files after the first file, append the results to the sf object that was created for the first file
 		} else{
 
 			altogether <- rbind(altogether, chunk)
@@ -65,5 +66,8 @@ for (i in c(start_i:end_i)) {
 }
 
 
+# write the combined "not_joined" shapefile to  single shapefile.
+st_write(altogether, file.path("./ar.2023.update.using.2021.pol.data/data/intermediate/1_population_and_colormap/4_export_notjoined_pop/not_joined/",
+                               "pop_notjoined_all.shp"), delete_layer = TRUE)
 
-st_write(altogether, file.path(pop_dir, "/", pop_year, "pop_notjoined_all.shp"), delete_layer = TRUE)
+# next step: join these unjoined shapefile in QGIS.
