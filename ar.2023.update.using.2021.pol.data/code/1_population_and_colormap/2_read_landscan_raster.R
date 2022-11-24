f48250ef19eaa1e746ea0f0370fbc39e0a995768
AarshBatra <aarshbatra.in@gmail.com>
2022-11-24 12:16:08 +0530

    Running the old workflow, post 1_shapefile_aggregate.R. Updated the 2_read_landscan.R file and accordingly modified folders given the new folder structure

--- ar.2023.update.using.2021.pol.data/code/1_population_and_colormap/2_read_landscan_raster.R
+++ ar.2023.update.using.2021.pol.data/code/1_population_and_colormap/2_read_landscan_raster.R
@@ -6,7 +6,7 @@
 
 # The raw LandScan data is a raster dataset.
 
-# Read it in, divide into 25 tiles and export 
+# Read it in, divide into 25 tiles and export
 
 # each as TIF file.
 
@@ -24,32 +24,26 @@ library(SpaDES)
 
 library(parallel)
 
-
-
-pop_year <- 2019
-
-
-
-ddir <- "C:/Arc/Preserve/data/population"                          #run on Acropolis
+library(stringr)
 
 
 
-landscan <- raster::raster(file.path(ddir, "/raw/landscan", pop_year, "/lspop", pop_year, fsep = ""))
+pop_year <- 2021
+
+# reading in landscan population raster for the pop_year
+landscan <- raster::raster( "./ar.2023.update.using.2021.pol.data/data/input/population/landscan-global-2021.tif")
 
 
 
 # Will split raster in parallel, using 4 cores or however many are available
 
 # cl <- pmin(parallel::detectCores(), 4)
-# 
+#
 # beginCluster(cl)
 
 # (Aarsh) I am not using the parallel cores option. I think it was messing with the splitting.
 
-tiles <- splitRaster(landscan, nx = 5, ny = 5, path = file.path(ddir, "intermediate/split_raster", pop_year))
-
-# endCluster()
-
+tiles <- splitRaster(landscan, nx = 5, ny = 5, path = "./ar.2023.update.using.2021.pol.data/data/intermediate/1_population_and_colormap/2_read_landscan_raster/split_raster")
 
 
 print("2_read_landscan_raster.R COMPLETED")
@@ -58,6 +52,6 @@ end_time <- Sys.time()
 
 elapsed_time <- end_time - start_time
 
-print(str_c("Elapsed Time: ", elapsed_time))
+print(str_c("Elapsed Time: ", elapsed_time)) # 2.33 minutes
 
 
